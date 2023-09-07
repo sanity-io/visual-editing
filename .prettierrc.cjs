41a8e29cbd89e326b6eb35cd0fca81004142e591
Marius Lundgård <studio@mariuslundgard.com>
2023-09-07 11:49:23 +0200

    chore: fix prettier config

--- .prettierrc.cjs
+++ .prettierrc.cjs
@@ -1,6 +1,5 @@
 module.exports = {
   semi: false,
   singleQuote: true,
-  // https://github.com/pnpm/pnpm/issues/3323
-  plugins: [require('prettier-plugin-packagejson')],
+  plugins: ['prettier-plugin-packagejson'],
 }
