023a7330f79b851c583c3127f526647fe4fa6b6e
Marius Lundgård <studio@mariuslundgard.com>
2023-09-14 14:25:01 +0200

    feat(composer): basic panels (#48)
    * chore(deps): install `@sanity/ui`
    
    * feat(composer): basic panels
    
    ---------
    
    Co-authored-by: Cody Olsen <81981+stipsan@users.noreply.github.com>

--- packages/composer/src/ComposerTool.tsx
+++ packages/composer/src/ComposerTool.tsx
@@ -1,7 +1,16 @@
+import { Card, Code, Flex } from '@sanity/ui'
 import { ReactElement, useEffect, useRef, useState } from 'react'
 import { isRecord, Tool } from 'sanity'
+import styled from 'styled-components'
 
 import { ComposerPluginOptions } from './types'
+
+const IFrame = styled.iframe`
+  border: 0;
+  height: 100%;
+  width: 100%;
+  display: block;
+`
 
 export default function ComposerTool(props: {
   tool: Tool<ComposerPluginOptions>
@@ -33,10 +42,15 @@ export default function ComposerTool(props: {
   }, [])
 
   return (
-    <div>
-      ComposerTool
-      <iframe ref={iframeRef} src={tool.options?.previewUrl || '/'}></iframe>
-      <pre>{JSON.stringify(log, null, 2)}</pre>
-    </div>
+    <Flex height="fill">
+      <Card flex={1}>
+        <IFrame ref={iframeRef} src={tool.options?.previewUrl || '/'} />
+      </Card>
+      <Card borderLeft flex={1} overflow="auto" padding={4}>
+        <Code language="json" size={1}>
+          {JSON.stringify(log, null, 2)}
+        </Code>
+      </Card>
+    </Flex>
   )
 }
