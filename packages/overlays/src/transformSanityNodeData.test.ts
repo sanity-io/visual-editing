5f5abc395963ba07c61d8297adecfd6c5ff6de6a
Rupert Dunk <rupert@rupertdunk.com>
2023-09-15 12:15:22 +0100

    test(overlays): add tests for encoding/decoding nodes

---
+++ packages/overlays/src/transformSanityNodeData.test.ts
@@ -0,0 +1,55 @@
+import { expect, test } from 'vitest'
+
+import {
+  decodeSanityString,
+  encodeSanityNodeData,
+} from './transformSanityNodeData'
+
+test('node data returns an encoded string', async () => {
+  const input = {
+    projectId: 'projectId',
+    dataset: 'dataset',
+    id: 'documentId',
+    path: 'sections[_key=="abcdef"].tagline',
+    baseUrl: 'https://some.sanity.studio',
+    tool: 'desk',
+    workspace: 'docs',
+  }
+
+  const output =
+    'project=projectId;dataset=dataset;id=documentId;path=sections:abcdef.tagline;base=https%3A%2F%2Fsome.sanity.studio;workspace=docs;tool=desk'
+
+  expect(encodeSanityNodeData(input)).toMatch(output)
+})
+
+test('incomplete node data returns undefined', async () => {
+  const input = {
+    projectId: 'projectId',
+    dataset: 'dataset',
+    id: 'documentId',
+    path: '',
+    baseUrl: 'https://some.sanity.studio',
+    tool: 'desk',
+    workspace: 'docs',
+  }
+
+  expect(encodeSanityNodeData(input)).toBeUndefined()
+})
+
+test('an encoded string returns node data', async () => {
+  const input = decodeSanityString(
+    'project=projectId;dataset=dataset;id=documentId;path=sections:abcdef.tagline;base=https%3A%2F%2Fsome.sanity.studio;workspace=docs;tool=desk',
+  )
+
+  const output = {
+    projectId: 'projectId',
+    dataset: 'dataset',
+    id: 'documentId',
+    path: 'sections[_key=="abcdef"].tagline',
+    baseUrl: 'https://some.sanity.studio',
+    tool: 'desk',
+    workspace: 'docs',
+  }
+
+  expect(input).toMatchObject(output)
+})
