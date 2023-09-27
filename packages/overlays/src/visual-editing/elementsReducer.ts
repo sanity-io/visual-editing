c1b56ddbdf95ff919a8467e434acb99d9896f446
Rupert Dunk <rupert@rupertdunk.com>
2023-09-27 10:27:26 +0100

    refactor(overlays): use switch statement in elements reducer

--- packages/overlays/src/visual-editing/elementsReducer.ts
+++ packages/overlays/src/visual-editing/elementsReducer.ts
@@ -6,8 +6,10 @@ export const elementsReducer = (
   message: OverlayMsg | ComposerMsg,
 ): ElementState[] => {
   const { type } = message
-  if (type === 'element/register') {
-    if (!elements.find((e) => e.id === message.id)) {
+  switch (type) {
+    case 'element/register': {
+      const elementExists = !!elements.find((e) => e.id === message.id)
+      if (elementExists) return elements
       return [
         ...elements,
         {
@@ -20,58 +22,59 @@ export const elementsReducer = (
         },
       ]
     }
-  } else if (type === 'element/activate') {
-    return elements.map((e) => {
-      if (e.id === message.id) {
-        return { ...e, activated: true }
-      }
-      return e
-    })
-  } else if (type === 'element/unregister') {
-    return elements.filter((e) => e.id !== message.id)
-  } else if (type === 'element/deactivate') {
-    return elements.map((e) => {
-      if (e.id === message.id) {
-        return { ...e, activated: false, hovered: false }
-      }
-      return e
-    })
-  } else if (type === 'element/mouseenter') {
-    return elements.map((e) => {
-      if (e.id === message.id) {
-        return { ...e, rect: message.rect, hovered: true }
-      }
-      return { ...e, hovered: false }
-    })
-  } else if (type === 'element/mouseleave') {
-    return elements.map((element) => {
-      if (element.id === message.id) {
-        return { ...element, hovered: false }
-      }
-      return element
-    })
-  } else if (type === 'element/updateRect') {
-    return elements.map((element) => {
-      if (element.id === message.id) {
-        return { ...element, rect: message.rect }
-      }
-      return element
-    })
-  } else if (type === 'element/click') {
-    return elements.map((e) => {
-      return { ...e, focused: e.id === message.id }
-    })
-  } else if (type === 'overlay/blur') {
-    return elements.map((e) => {
-      return { ...e, focused: false }
-    })
-  } else if (type === 'composer/focus') {
-    return elements.map((e) => {
-      return {
-        ...e,
-        focused: 'path' in e.sanity && e.sanity.path === message.data.path[0],
-      }
-    })
+    case 'element/activate':
+      return elements.map((e) => {
+        if (e.id === message.id) {
+          return { ...e, activated: true }
+        }
+        return e
+      })
+    case 'element/unregister':
+      return elements.filter((e) => e.id !== message.id)
+    case 'element/deactivate':
+      return elements.map((e) => {
+        if (e.id === message.id) {
+          return { ...e, activated: false, hovered: false }
+        }
+        return e
+      })
+    case 'element/mouseenter':
+      return elements.map((e) => {
+        if (e.id === message.id) {
+          return { ...e, rect: message.rect, hovered: true }
+        }
+        return { ...e, hovered: false }
+      })
+    case 'element/mouseleave':
+      return elements.map((element) => {
+        if (element.id === message.id) {
+          return { ...element, hovered: false }
+        }
+        return element
+      })
+    case 'element/updateRect':
+      return elements.map((element) => {
+        if (element.id === message.id) {
+          return { ...element, rect: message.rect }
+        }
+        return element
+      })
+    case 'element/click':
+      return elements.map((e) => {
+        return { ...e, focused: e.id === message.id }
+      })
+    case 'overlay/blur':
+      return elements.map((e) => {
+        return { ...e, focused: false }
+      })
+    case 'composer/focus':
+      return elements.map((e) => {
+        return {
+          ...e,
+          focused: 'path' in e.sanity && e.sanity.path === message.data.path[0],
+        }
+      })
+    default:
+      return elements
   }
-  return elements
 }
