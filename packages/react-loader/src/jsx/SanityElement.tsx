281949ffbb417f7f0a6268f811cba5bc1c10fce4
Marius Lundgård <studio@mariuslundgard.com>
2023-10-17 15:20:37 +0200

    feat(react-loader): add JSX helper

---
+++ packages/react-loader/src/jsx/SanityElement.tsx
@@ -0,0 +1,30 @@
+import type { SourceNode } from '@sanity/csm'
+import { encodeSanityNodeData } from '@sanity/overlays'
+import type { HTMLProps, Ref } from 'react'
+import { createElement, forwardRef } from 'react'
+
+export interface SanityElementProps {
+  children?: SourceNode | null
+}
+
+export const SanityElement = forwardRef(function SanityElement(
+  props: { as: string } & SanityElementProps &
+    Omit<HTMLProps<HTMLElement>, 'children'>,
+  ref: Ref<HTMLElement | SVGElement>,
+) {
+  const { as, children: node, ...restProps } = props
+
+  if (node?.source) {
+    return createElement(
+      as,
+      {
+        ...restProps,
+        'data-sanity': encodeSanityNodeData(node.source),
+        ref,
+      },
+      node.value,
+    )
+  }
+
+  return createElement(as, restProps, node?.value)
+})
