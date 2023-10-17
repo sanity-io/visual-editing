f2f8b8a427e76775b92a19d0262ea5fa21dad609
Cody Olsen <stipsan@gmail.com>
2023-10-17 17:56:16 +0200

    chore: no more token auth

--- packages/composer/src/useLoaders.ts
+++ packages/composer/src/useLoaders.ts
@@ -4,7 +4,6 @@
 
 import type { ClientPerspective } from '@sanity/client'
 import { ChannelReturns } from 'channels'
-import { useEffect } from 'react'
 import { useClient } from 'sanity'
 import { VisualEditingMsg } from 'visual-editing-helpers'
 
@@ -12,36 +11,10 @@ export function useLoaders(props: {
   channel: ChannelReturns<VisualEditingMsg> | undefined
   perspective: ClientPerspective
 }): void {
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
   const { channel, perspective } = props
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
   const client = useClient({ apiVersion: '2023-10-16' })
 
-  // Set the preview perspective, and send over the token needed to preview drafts
-  useEffect(() => {
-    // @TODO handle this in a better way in https://linear.app/sanity/issue/ECO-174/handle-tokens-auth-in-a-consistent-way
-    const { token, projectId, dataset } = client.config()
-    if (!token) {
-      throw new Error(
-        'No token found in client config, try logging out and back in again',
-      )
-    }
-    if (!projectId) {
-      throw new Error('No projectId found in client config')
-    }
-    if (!dataset) {
-      throw new Error('No dataset found in client config')
-    }
-
-    console.log('loader/perspective', {
-      projectId,
-      dataset,
-      perspective,
-      token,
-    })
-    channel?.send('loader/perspective', {
-      projectId,
-      dataset,
-      perspective,
-      token,
-    })
-  }, [channel, client, perspective])
+  // @TODO
 }
