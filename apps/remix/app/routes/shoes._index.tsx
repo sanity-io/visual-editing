e48ae5d39f91d08b49d9845b6b0219bef86cf67f
Cody Olsen <stipsan@gmail.com>
2023-10-18 01:10:05 +0200

    chore: fix exception

--- apps/remix/app/routes/shoes._index.tsx
+++ apps/remix/app/routes/shoes._index.tsx
@@ -56,7 +56,7 @@ export default function ProductsRoute() {
           <div className="animate-pulse">Loading...</div>
         ) : (
           <div className="grid grid-cols-1 gap-x-6 gap-y-10 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 xl:gap-x-8">
-            {products?.map((product, i) => (
+            {products?.map?.((product, i) => (
               <Link
                 key={product.slug.current.value}
                 to={`/shoes/${product.slug.current.value}`}
