95663925be866869b73c1e9a17ccf3b7b7265423
Marius Lundgård <studio@mariuslundgard.com>
2023-10-17 16:53:58 +0200

    fix(studio): simplify `locate` resolver

--- apps/studio/locate.ts
+++ apps/studio/locate.ts
@@ -5,33 +5,25 @@ export const locate: DocumentLocationResolver = (params, context) => {
   const { documentStore } = context
 
   if (params.type === 'shoe') {
-    const queryParams = {
-      id: params.id,
-      draftId: `drafts.${params.id}`,
-    }
-
     // Listen to the query and fetch the draft and published document
-    const doc$ = documentStore
-      .listenQuery(
-        {
-          listen: `*[(_id == $id || _id == $draftId) && defined(slug)]`,
-          fetch: `{"draft":*[_id==$draftId]{slug,title}[0],"published":*[_id==$id]{slug,title}[0]}`,
-        },
-        queryParams,
-        {},
-      )
-      .pipe(map((result) => result.draft || result.published)) as Observable<{
-      slug: { current: string }
+    const doc$ = documentStore.listenQuery(
+      `*[_id == $id][0]{slug,title}`,
+      params,
+      { perspective: 'previewDrafts' },
+    ) as Observable<{
+      slug: { current: string | null } | null
       title: string | null
     } | null>
 
     return doc$.pipe(
       map((doc) => {
+        if (!doc || !doc.slug?.current) return null
+
         return {
           locations: [
             {
-              title: doc?.title || 'Untitled',
-              href: `/shoes/${doc?.slug?.current}`,
+              title: doc.title || 'Untitled',
+              href: `/shoes/${doc.slug.current}`,
             },
             {
               title: 'Shoes',
