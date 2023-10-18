e44ec2334f365055a29edcadaa1ffa64bc9a366f
Cody Olsen <stipsan@gmail.com>
2023-10-18 12:55:30 +0200

    chore: enable strict mode in next

--- apps/next/next.config.mjs
+++ apps/next/next.config.mjs
@@ -2,6 +2,7 @@ import withBundleAnalyzer from '@next/bundle-analyzer'
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  reactStrictMode: true,
   experimental: {
     logging: {
       level: 'verbose',
