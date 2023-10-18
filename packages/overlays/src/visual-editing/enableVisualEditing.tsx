ba2a5af34e85a818434df1e795490fad7f2b095a
Cody Olsen <stipsan@gmail.com>
2023-10-18 12:22:02 +0200

    fix: React StrictMode race conditions

--- packages/overlays/src/visual-editing/enableVisualEditing.tsx
+++ packages/overlays/src/visual-editing/enableVisualEditing.tsx
@@ -10,19 +10,7 @@ export type DisableVisualEditing = () => void
 
 let node: HTMLElement | null = null
 let root: Root | null = null
-
-function cleanup() {
-  if (root) {
-    root.unmount()
-    root = null
-  }
-  if (node) {
-    if (document.body.contains(node)) {
-      document.body.removeChild(node)
-    }
-    node = null
-  }
-}
+let cleanup: number | null = null
 
 /**
  * Enables Visual Editing overlay in a page with sourcemap encoding.
@@ -38,24 +26,29 @@ export function enableVisualEditing(
     zIndex?: string | number
   } = {},
 ): DisableVisualEditing {
-  if (root || node) return cleanup
-  let cancelled = false
+  if (cleanup) clearTimeout(cleanup)
+  const controller = new AbortController()
 
   // Lazy load everything needed to render the app
   Promise.all([import('react-dom/client'), import('./VisualEditing')]).then(
     ([reactClient, { VisualEditing }]) => {
-      if (cancelled) return
+      if (controller.signal.aborted) return
 
       const { history, zIndex } = options
       const studioUrl = options.studioUrl || parent.origin
 
-      node = document.createElement('div')
-      node.id = OVERLAY_ID
-      document.body.appendChild(node)
+      if (!node) {
+        node = document.createElement('div')
+        node.id = OVERLAY_ID
+        document.body.appendChild(node)
+      }
 
-      const { createRoot } =
-        'default' in reactClient ? reactClient.default : reactClient
-      root = createRoot(node)
+      if (!root) {
+        const { createRoot } =
+          'default' in reactClient ? reactClient.default : reactClient
+        root = createRoot(node)
+      }
+
       root.render(
         <VisualEditing
           history={history}
@@ -67,7 +60,17 @@ export function enableVisualEditing(
   )
 
   return () => {
-    cancelled = true
-    cleanup()
+    controller.abort()
+    // Handle React StrictMode, delay cleanup for a second in case it's a rerender
+    cleanup = window.setTimeout(() => {
+      if (root) {
+        root.unmount()
+        root = null
+      }
+      if (node) {
+        document.body.removeChild(node)
+        node = null
+      }
+    }, 1000)
   }
 }
