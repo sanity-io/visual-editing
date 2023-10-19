b8090e932975772193535dd20c63b2e80b445033
Marius Lundgård <studio@mariuslundgard.com>
2023-10-19 15:39:43 +0200

    fix(apps): fonts

--- apps/page-builder-demo/src/app/layout.tsx
+++ apps/page-builder-demo/src/app/layout.tsx
@@ -3,24 +3,6 @@ import type { Metadata } from 'next'
 import { IBM_Plex_Mono, Inter, PT_Serif } from 'next/font/google'
 import VisualEditing from './VisualEditing'
 
-const mono = IBM_Plex_Mono({
-  variable: '--font-mono',
-  subsets: ['latin'],
-  weight: ['500', '700'],
-})
-
-const sans = Inter({
-  variable: '--font-sans',
-  subsets: ['latin'],
-  weight: ['500', '700', '800'],
-})
-
-const serif = PT_Serif({
-  variable: '--font-serif',
-  subsets: ['latin'],
-  weight: ['400', '700'],
-})
-
 export const metadata: Metadata = {
   title: '',
   description: '',
@@ -33,9 +15,20 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body
-        className={`${mono.className} ${sans.className} ${serif.className} bg-white text-black dark:bg-black dark:text-white`}
-      >
+      <head>
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link
+          rel="preconnect"
+          href="https://fonts.gstatic.com"
+          crossOrigin=""
+        />
+        <link
+          href="https://fonts.googleapis.com/css2?family=IBM+Plex+Mono:wght@500;700&family=Inter:wght@500;700;800&family=PT+Serif:ital,wght@0,400;0,700;1,400;1,700&display=swap"
+          rel="stylesheet"
+        />
+      </head>
+
+      <body className="bg-white text-black dark:bg-black dark:text-white">
         {children}
       </body>
 
