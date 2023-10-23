6a0dbf082dba9876ede396d1b8f2ceaafa67e924
Cody Olsen <stipsan@gmail.com>
2023-10-23 22:46:07 +0200

    fix: better bundling

--- packages/composer/src/useParams.ts
+++ packages/composer/src/useParams.ts
@@ -1,4 +1,4 @@
-import { isEqual } from 'lodash'
+import isEqual from 'lodash.isequal'
 import { useCallback, useEffect, useMemo, useRef, useState } from 'react'
 import { stringToPath } from 'sanity'
 import {
