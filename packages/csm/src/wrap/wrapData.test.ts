22e53e7c774e99b1187a7c0700a216ec23e20434
Marius Lundgård <studio@mariuslundgard.com>
2023-10-25 14:16:59 +0200

    test(csm): update test

--- packages/csm/src/wrap/wrapData.test.ts
+++ packages/csm/src/wrap/wrapData.test.ts
@@ -18,14 +18,15 @@ test('wrap an array of documents with source maps', () => {
   expect(wrapped?.[0]).toEqual({
     _id: '0e6fa235-3bd5-41cc-9f25-53dc0a5ff7d2',
     title: {
-      value: 'Nike Pegasus 39 Shield',
       $$type$$: 'sanity',
+      path: '[0].title',
       source: {
         ...context,
         id: '0e6fa235-3bd5-41cc-9f25-53dc0a5ff7d2',
         type: 'shoe',
         path: 'title',
       },
+      value: 'Nike Pegasus 39 Shield',
     },
   })
 })
@@ -44,6 +45,7 @@ test('wrap a deep target with source maps', () => {
 
   expect(wrapped?.seo?.og?.title).toEqual({
     $$type$$: 'sanity',
+    path: 'seo.og.title',
     source: {
       ...context,
       id: '80ddcd8a-a89f-4163-8f8d-b941787da6e0',
