28be3e965fc54ef6cd213d15e99e90c0acfb962a
Marius Lundgård <studio@mariuslundgard.com>
2023-11-08 17:56:41 +0100

    fixup! feat(presentation): add 'Open in Desk' action

--- packages/presentation/src/fieldActions/openInDesk.ts
+++ packages/presentation/src/fieldActions/openInDesk.ts
@@ -1,6 +1,10 @@
 import { MasterDetailIcon } from '@sanity/icons'
 import { useContext } from 'react'
-import { defineDocumentFieldAction } from 'sanity'
+import {
+  defineDocumentFieldAction,
+  DocumentFieldActionItem,
+  pathToString,
+} from 'sanity'
 import { useDocumentPane } from 'sanity/desk'
 import { useRouter } from 'sanity/router'
 
@@ -8,8 +12,8 @@ import { PresentationContext } from '../PresentationContext'
 
 export const openInDesk = defineDocumentFieldAction({
   name: 'presentation/openInDesk',
-  useAction() {
-    const { documentId, documentType } = useDocumentPane()
+  useAction({ documentId, path }) {
+    const { documentType } = useDocumentPane()
     const { navigateIntent } = useRouter()
     const presentation = useContext(PresentationContext)
 
@@ -23,9 +27,10 @@ export const openInDesk = defineDocumentFieldAction({
           id: documentId,
           type: documentType,
           tool: 'desk',
+          path: pathToString(path),
         } as any)
       },
       renderAsButton: true,
-    }
+    } satisfies DocumentFieldActionItem
   },
 })
