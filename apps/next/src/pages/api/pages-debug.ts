91a4598984b62a56f3a0510fa359349ba7bc5cf0
Cody Olsen <stipsan@gmail.com>
2023-11-10 13:58:04 +0100

    chore: add debug endpoint

---
+++ apps/next/src/pages/api/pages-debug.ts
@@ -0,0 +1,8 @@
+import type { NextApiRequest, NextApiResponse } from 'next'
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse<string>,
+) {
+  return res.send(req.url!)
+}
