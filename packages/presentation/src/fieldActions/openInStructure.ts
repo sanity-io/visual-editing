f61705210035a151671899d0cfc4dd69e8b0e994
Marius Lundgård <studio@mariuslundgard.com>
2023-11-13 13:12:22 +0100

    fix(presentation): render icon and title of Structure tool in field action

--- packages/presentation/src/fieldActions/openInStructure.ts
+++ packages/presentation/src/fieldActions/openInStructure.ts
@@ -1,9 +1,12 @@
 import { MasterDetailIcon } from '@sanity/icons'
-import { useContext } from 'react'
+import { useContext, useMemo } from 'react'
 import {
   defineDocumentFieldAction,
   DocumentFieldActionItem,
+  isRecord,
   pathToString,
+  Tool,
+  useWorkspace,
 } from 'sanity'
 import { useRouter } from 'sanity/router'
 
@@ -12,23 +15,55 @@ import { PresentationContext } from '../PresentationContext'
 export const openInStructure = defineDocumentFieldAction({
   name: 'presentation/openInStructure',
   useAction({ documentId, documentType, path }) {
+    const workspace = useWorkspace()
     const { navigateIntent } = useRouter()
     const presentation = useContext(PresentationContext)
 
+    const defaultStructureTool = useMemo(
+      () => findStructureTool(workspace.tools),
+      [workspace.tools],
+    )
+
     return {
       type: 'action',
-      hidden: !presentation || path.length > 0,
-      icon: MasterDetailIcon,
-      title: 'Open in Structure',
+      hidden: !presentation || path.length > 0 || !defaultStructureTool,
+      icon: defaultStructureTool?.icon || MasterDetailIcon,
+      title: `Open in ${defaultStructureTool?.title || 'Structure'}`,
       onAction() {
         navigateIntent('edit', {
           id: documentId,
           type: documentType,
           mode: 'structure',
           path: pathToString(path),
-        } as any)
+        })
       },
       renderAsButton: true,
     } satisfies DocumentFieldActionItem
   },
 })
+
+function findStructureTool(tools: Tool[]): Tool | undefined {
+  const results = tools.map((t) => {
+    const match = t.canHandleIntent?.(
+      'edit',
+      {
+        id: 'check',
+        type: 'check',
+        mode: 'structure',
+      },
+      {},
+    )
+
+    return { tool: t, match }
+  })
+
+  const modeMatches = results.filter((t) => isRecord(t.match) && t.match.mode)
+
+  if (modeMatches.length === 1) {
+    return modeMatches[0].tool
+  }
+
+  const matches = results.filter((t) => t.match)
+
+  return matches[0]?.tool
+}
