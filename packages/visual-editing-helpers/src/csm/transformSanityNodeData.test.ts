f1b5b83ac3a8cd0ba0d9f8fe6fe4976f5b35cd1d
Cody Olsen <stipsan@gmail.com>
2023-11-13 21:18:58 +0100

    fix: update tests

--- packages/visual-editing-helpers/src/csm/transformSanityNodeData.test.ts
+++ packages/visual-editing-helpers/src/csm/transformSanityNodeData.test.ts
@@ -7,8 +7,6 @@ import {
 
 test('node data returns an encoded string', async () => {
   const input = {
-    projectId: 'projectId',
-    dataset: 'dataset',
     id: 'documentId',
     type: 'documentType',
     path: 'sections[_key=="abcdef"].tagline',
@@ -18,15 +16,13 @@ test('node data returns an encoded string', async () => {
   }
 
   const output =
-    'project=projectId;dataset=dataset;id=documentId;type=documentType;path=sections:abcdef.tagline;base=https%3A%2F%2Fsome.sanity.studio;workspace=docs;tool=desk'
+    'id=documentId;type=documentType;path=sections:abcdef.tagline;base=https%3A%2F%2Fsome.sanity.studio;workspace=docs;tool=desk'
 
   expect(encodeSanityNodeData(input)).toMatch(output)
 })
 
 test('incomplete node data returns undefined', async () => {
   const input = {
-    projectId: 'projectId',
-    dataset: 'dataset',
     id: 'documentId',
     type: 'documentType',
     path: '',
@@ -40,12 +36,10 @@ test('incomplete node data returns undefined', async () => {
 
 test('an encoded string returns node data', async () => {
   const input = decodeSanityString(
-    'project=projectId;dataset=dataset;id=documentId;type=documentType;path=sections:abcdef.tagline;base=https%3A%2F%2Fsome.sanity.studio;workspace=docs;tool=desk',
+    'id=documentId;type=documentType;path=sections:abcdef.tagline;base=https%3A%2F%2Fsome.sanity.studio;workspace=docs;tool=desk',
   )
 
   const output = {
-    projectId: 'projectId',
-    dataset: 'dataset',
     id: 'documentId',
     type: 'documentType',
     path: 'sections[_key=="abcdef"].tagline',
