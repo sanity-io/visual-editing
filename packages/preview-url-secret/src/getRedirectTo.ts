03591b50d60675d3d1a0eed1b66c7e528a63a1b7
Cody Olsen <stipsan@gmail.com>
2023-11-17 00:58:40 +0100

    feat: add `getRedirectTo` utility

---
+++ packages/preview-url-secret/src/getRedirectTo.ts
@@ -0,0 +1,15 @@
+import { urlSearchParamPreviewPathname } from './constants'
+
+/**
+ * @internal
+ */
+export function getRedirectTo(url: URL): URL {
+  if (url.searchParams.has(urlSearchParamPreviewPathname)) {
+    return new URL(
+      url.searchParams.get(urlSearchParamPreviewPathname)!,
+      url.origin,
+    )
+  }
+
+  return url
+}
