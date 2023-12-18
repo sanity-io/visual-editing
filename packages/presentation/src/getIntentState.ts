772b7d00ee195cc8053180b2c3df570a9295e505
rdunk <rupert@rupertdunk.com>
2023-12-18 16:07:59 +0000

    fix(presentation): include id in intent state

--- packages/presentation/src/getIntentState.ts
+++ packages/presentation/src/getIntentState.ts
@@ -10,18 +10,17 @@ export function getIntentState(
   _routerState: undefined,
   payload: unknown,
 ):
-  | { type: string; path: string; _searchParams: SearchParam[] }
+  | { id: string; type: string; path: string; _searchParams: SearchParam[] }
   | { intent: string; params: Record<string, string>; payload: unknown } {
   // eslint-disable-next-line @typescript-eslint/no-unused-vars
   const { id, mode, path, presentation, type, ...searchParams } = params
-
   if (intent === 'edit' && id) {
     return {
       type: type || '*',
-      path: [getPublishedId(id), path].filter(Boolean).join('.'),
+      id: getPublishedId(id),
+      path,
       _searchParams: Object.entries(searchParams),
     }
   }
-
   return { intent, params, payload }
 }
