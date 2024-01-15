b4290a145f994d0fb722fa2c85c28bb2ddde71f8
rdunk <rupert@rupertdunk.com>
2024-01-15 18:55:32 +0000

    chore(apps): product page parity

--- apps/svelte/src/hooks.server.ts
+++ apps/svelte/src/hooks.server.ts
@@ -7,9 +7,9 @@ setServerClient(
     token: SANITY_API_READ_TOKEN,
     useCdn: false,
     perspective: 'previewDrafts',
-    // stega: {
-    //   ...client.config().stega,
-    //   enabled: process.env.NEXT_PUBLIC_VERCEL_ENV === 'preview',
-    // },
+    stega: {
+      ...client.config().stega,
+      enabled: true,
+    },
   }),
 )
