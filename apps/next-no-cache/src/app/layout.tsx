06e62524e7380867ceb9b42fade563fefdc96cd1
Cody Olsen <stipsan@gmail.com>
2024-01-23 09:47:39 +0100

    chore: add speed insights

--- apps/next-no-cache/src/app/layout.tsx
+++ apps/next-no-cache/src/app/layout.tsx
@@ -2,6 +2,7 @@ import 'tailwindcss/tailwind.css'
 
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
+import { SpeedInsights } from '@vercel/speed-insights/next'
 
 import { CMS_NAME } from '@/lib/constants'
 
@@ -23,7 +24,10 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en" className={inter.variable}>
-      <body className="bg-white text-black">{children}</body>
+      <body className="bg-white text-black">
+        {children}
+        <SpeedInsights />
+      </body>
     </html>
   )
 }
