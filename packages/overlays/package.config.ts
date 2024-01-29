8a5f7992226bad944890425cab1aad085ebcd68c
Cody Olsen <stipsan@gmail.com>
2024-01-30 00:03:18 +0100

    fix: improve bundling

--- packages/overlays/package.config.ts
+++ packages/overlays/package.config.ts
@@ -9,23 +9,4 @@ export default defineConfig({
   define: {
     'process.env.NODE_ENV': 'production',
   },
-  external: (prev) => [
-    ...prev.filter(
-      (dep) => dep !== '@sanity/ui' && dep !== 'styled-components',
-    ),
-    // deps used by '@sanity/ui',
-    '@floating-ui/react-dom',
-    '@sanity/color',
-    '@sanity/icons',
-    // 'framer-motion', commented out as it creates a junk import
-    'react-refractor',
-    'react-is',
-
-    // deps used by 'styled-components',
-    '@emotion/is-prop-valid',
-    '@emotion/unitless',
-    // 'shallowequal', commented out as it creates a junk import
-    'stylis',
-    'tslib',
-  ],
 })
