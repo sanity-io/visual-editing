75fdf55377aebceba8a484ea5ba01df257d1a765
Cody Olsen <stipsan@gmail.com>
2024-02-06 22:38:29 +0100

    chore: use `VisualEditing` from `next-sanity`

--- apps/page-builder-demo/src/app/VisualEditing.tsx
+++ apps/page-builder-demo/src/app/VisualEditing.tsx
@@ -1,58 +1,10 @@
 'use client'
 
 import { useLiveMode } from '@/sanity'
-import {
-  enableVisualEditing,
-  HistoryAdapterNavigate,
-} from '@sanity/visual-editing'
-import { studioUrl } from 'apps-common/env'
-import { useEffect, useRef, useState } from 'react'
-import { usePathname, useSearchParams, useRouter } from 'next/navigation'
+import { VisualEditing } from 'next-sanity'
 
-export default function VisualEditing() {
-  const router = useRouter()
-  const routerRef = useRef(router)
-  const [navigate, setNavigate] = useState<HistoryAdapterNavigate | undefined>()
-
-  useEffect(() => {
-    routerRef.current = router
-  }, [router])
-  useEffect(() => {
-    const disable = enableVisualEditing({
-      history: {
-        subscribe: (navigate) => {
-          setNavigate(() => navigate)
-          return () => setNavigate(undefined)
-        },
-        update: (update) => {
-          switch (update.type) {
-            case 'push':
-              return routerRef.current.push(update.url)
-            case 'pop':
-              return routerRef.current.back()
-            case 'replace':
-              return routerRef.current.replace(update.url)
-            default:
-              throw new Error(`Unknown update type: ${update.type}`)
-          }
-        },
-      },
-    })
-    return () => disable()
-  }, [])
-
-  const pathname = usePathname()
-  const searchParams = useSearchParams()
-  useEffect(() => {
-    if (navigate) {
-      navigate({
-        type: 'push',
-        url: `${pathname}${searchParams?.size ? `?${searchParams}` : ''}`,
-      })
-    }
-  }, [navigate, pathname, searchParams])
-
+export default function LiveVisualEditing() {
   useLiveMode({})
 
-  return null
+  return <VisualEditing />
 }
