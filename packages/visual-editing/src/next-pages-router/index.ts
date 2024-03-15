7fca31223977bd6ea2dfdea7997cfa4c867243fa
Cody Olsen <stipsan@gmail.com>
2024-03-15 14:07:20 +0100

    feat(visual-editing): add `createDataAttribute` to `remix` and `next-pages-router`

--- packages/visual-editing/src/next-pages-router/index.ts
+++ packages/visual-editing/src/next-pages-router/index.ts
@@ -7,3 +7,8 @@ export type {
 } from '../types'
 export { VisualEditing } from './VisualEditing'
 export type { VisualEditingProps } from './VisualEditingComponent'
+export {
+  type CreateDataAttribute,
+  createDataAttribute,
+  type CreateDataAttributeProps,
+} from '@sanity/visual-editing-helpers'
