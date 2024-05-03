0e402b8dfc2efcae583c332b06401f3a912be036
rdunk <rupert@rupertdunk.com>
2024-05-03 13:40:34 +0100

    refactor(presentation): restyle main document badge

--- packages/presentation/src/editor/ContentEditor.tsx
+++ packages/presentation/src/editor/ContentEditor.tsx
@@ -1,5 +1,5 @@
 import {WarningOutlineIcon} from '@sanity/icons'
-import {Badge, Box, Card, Flex, Text} from '@sanity/ui'
+import {Box, Card, Flex, Text} from '@sanity/ui'
 import {type HTMLProps, type ReactElement, useCallback} from 'react'
 import {type Path, Translate, useSchema, useTranslation} from 'sanity'
 import {StateLink} from 'sanity/router'
@@ -71,7 +71,13 @@ export function ContentEditor(props: {
             <Card as={MainDocumentLink} data-as="a" padding={0} radius={2}>
               <Preview
                 schemaType={schema.get(mainDocumentState.document._type)!}
-                status={<Badge>{t('main-document.label')}</Badge>}
+                status={
+                  <Card padding={1} radius={2} shadow={1}>
+                    <Text muted size={0} weight="medium">
+                      {t('main-document.label')}
+                    </Text>
+                  </Card>
+                }
                 value={mainDocumentState.document}
               />
             </Card>
