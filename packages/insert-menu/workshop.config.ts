c3716dd8604d438011f12fe831a135c19e0418c6
Marius Lundgård <studio@mariuslundgard.com>
2024-06-13 12:35:50 +0200

    chore(insert-menu): set up workshop

---
+++ packages/insert-menu/workshop.config.ts
@@ -0,0 +1,7 @@
+import {buildTheme} from '@sanity/ui/theme'
+import {defineConfig} from '@sanity/ui-workshop'
+
+export default defineConfig({
+  theme: buildTheme(),
+  title: '@sanity/insert-menu',
+})
