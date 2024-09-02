f6806badbc5956c15bc099d1972e55de2477954c
Cody Olsen <stipsan@gmail.com>
2024-09-02 17:10:38 +0200

    fix: remove duplicate sc warning

--- packages/visual-editing/package.config.ts
+++ packages/visual-editing/package.config.ts
@@ -7,6 +7,9 @@ export default defineConfig({
   runtime: 'browser',
   define: {
     'process.env.NODE_ENV': 'production',
+    // Since we embed `styled-components` as an implementation detail, setting this to a different value ensures userland won't see "multiple versions of styled-components`"" warnings
+    // https://github.com/styled-components/styled-components/blob/6f6db180bd0ec89bd4342dc4b8f1eae0b34d8dca/packages/styled-components/src/constants.ts#L4-L8
+    'process.env.SC_ATTR': 'data-styled-sanity',
   },
   rollup: {
     treeshake: {
