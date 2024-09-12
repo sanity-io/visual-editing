a29e26fde2e5826ff938e179920df1bd8ac79462
George Francis <30405123+georgedoescode@users.noreply.github.com>
2024-09-12 18:13:52 +0100

    chore(prevent-dnd-in-non-studio-context) (#1817)
    * chore(prevent-dnd-in-non-studio-context): disable dnd outside frame
    
    * chore(prevent-dnd-in-non-studio-context): pass inFrame to createOverlayController

--- packages/visual-editing/src/types.ts
+++ packages/visual-editing/src/types.ts
@@ -190,7 +190,7 @@ export type OverlayEventHandler = (message: OverlayMsg) => void
 export interface OverlayOptions {
   handler: OverlayEventHandler
   overlayElement: HTMLElement
-  preventDefault: boolean
+  inFrame: boolean
 }
 
 /**
