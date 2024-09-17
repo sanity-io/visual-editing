d7773a0b719f6bb4ad69df72378ef348fa21ddfd
rdunk <rupert@rupertdunk.com>
2024-09-17 11:18:13 +0100

    fix(visual-editing): support dragging on all axes

--- packages/visual-editing/src/util/useDragEvents.ts
+++ packages/visual-editing/src/util/useDragEvents.ts
@@ -2,11 +2,27 @@ import {at, createIfNotExists, insert, patch, remove} from '@sanity/mutate'
 import {get} from '@sanity/util/paths'
 import {useCallback, useEffect} from 'react'
 
-import type {DragEndEvent} from '../types'
+import type {DragEndEvent, DragInsertPosition} from '../types'
 import {isEmptyActor} from '../ui/optimistic-state/context'
 import {useOptimisticActor} from '../ui/optimistic-state/useOptimisticActor'
 import {getDraftId} from './documents'
 import {getArrayItemKeyAndParentPath} from './mutations'
+
+// Finds the node that the drag end event was relative to, and the relative
+// position the new element should be inserted in. If the reference node was
+// "top" or "left", we insert "after". If it was "bottom" or "right", we insert
+// "before".
+function getReferenceNodeAndInsertPosition(position: DragInsertPosition) {
+  if (position) {
+    const {top, right, bottom, left} = position
+    if (left || top) {
+      return {node: (left ?? top)!.sanity, position: 'after' as const}
+    } else if (right || bottom) {
+      return {node: (right ?? bottom)!.sanity, position: 'before' as const}
+    }
+  }
+  return undefined
+}
 
 export function useDragEndEvents(): {
   dispatchDragEndEvent: (event: DragEndEvent) => void
@@ -19,29 +35,35 @@ export function useDragEndEvents(): {
     }
     const handler = (e: CustomEvent<DragEndEvent>) => {
       const {insertPosition, target} = e.detail
-      if (insertPosition?.top) {
+      const reference = getReferenceNodeAndInsertPosition(insertPosition)
+      if (reference) {
         const id = getDraftId(target.id)
-        const snapshotContext = actor.getSnapshot().context
-        const doc = snapshotContext.documents[id]
-        if (!doc) return
-        const documentSnapshot = doc.getSnapshot().context.local
-        if (!documentSnapshot) return
-        const elementValue = get(documentSnapshot, target.path)
-        const {key: targetKey} = getArrayItemKeyAndParentPath(target.path)
-        const {path: arrayPath, key: referenceItemKey} = getArrayItemKeyAndParentPath(
-          insertPosition.top.sanity,
-        )
-        if (referenceItemKey && referenceItemKey !== targetKey && arrayPath) {
+        const doc = actor.getSnapshot().context.documents[id]
+        const snapshot = doc?.getSnapshot().context.local
+        // We must have access to the document actor and snapshot in order to
+        // perform the necessary mutations. If either of these are undefined,
+        // something went wrong when resolving the currently in use documents
+        if (!doc || !snapshot) return
+
+        const {node, position} = reference
+        // Get the current value of the element we dragged, as we will need to
+        // "clone" this into the new position
+        const elementValue = get(snapshot, target.path)
+        // Get the key of the element that was dragged
+        const {key: targetKey} = getArrayItemKeyAndParentPath(target)
+        // Get the key of the reference element, and path to the parent array
+        const {path: arrayPath, key: referenceItemKey} = getArrayItemKeyAndParentPath(node)
+        // Don't perform a mutation if the keys match, as this means the item
+        // was only dragged to its existing position, i.e. not moved
+        if (arrayPath && referenceItemKey && referenceItemKey !== targetKey) {
           const mutations = [
             createIfNotExists({_id: id, _type: target.type!}),
+            // Remove the original dragged item
             patch(id, at(arrayPath, remove({_key: targetKey}))),
-            patch(id, at(arrayPath, insert(elementValue, 'after', {_key: referenceItemKey}))),
+            // Insert the cloned dragged item into its new position
+            patch(id, at(arrayPath, insert(elementValue, position, {_key: referenceItemKey}))),
           ]
-
-          doc.send({
-            type: 'mutate',
-            mutations: mutations,
-          })
+          doc.send({type: 'mutate', mutations: mutations})
           doc.send({type: 'submit'})
         }
       }
