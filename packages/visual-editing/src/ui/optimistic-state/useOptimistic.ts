99e2f4d387915922d1e91932308fc948682dbc33
rdunk <rupert@rupertdunk.com>
2024-09-18 00:23:04 +0100

    fix(visual-editing): use ref for reducers passed to useOptimistic hook

--- packages/visual-editing/src/ui/optimistic-state/useOptimistic.ts
+++ packages/visual-editing/src/ui/optimistic-state/useOptimistic.ts
@@ -1,6 +1,6 @@
 import type {SanityDocumentBase} from '@sanity/mutate'
 import type {SanityDocument} from '@sanity/types'
-import {useCallback, useEffect, useState} from 'react'
+import {useCallback, useEffect, useRef, useState} from 'react'
 import {getPublishedId} from '../../util/documents'
 import {isEmptyActor, type OptimisticReducer, type OptimisticReducerAction} from './context'
 import {useOptimisticActor} from './useOptimisticActor'
@@ -13,27 +13,31 @@ export function useOptimistic<T, U = SanityDocument>(
 
   const actor = useOptimisticActor()
 
-  const setStateFromAction = useCallback(
-    (action: OptimisticReducerAction<U>) => {
-      const reducers = Array.isArray(reducer) ? reducer : [reducer]
-      setState((prevState) => {
-        return reducers.reduce(
-          (acc, reducer) =>
-            reducer(acc, {type: action.type, document: action.document, id: action.id}),
-          prevState,
-        )
-      })
-    },
-    [reducer],
-  )
+  const reducerRef = useRef(reducer)
+  useEffect(() => {
+    reducerRef.current = reducer
+  }, [reducer])
+
+  const setStateFromAction = useCallback((action: OptimisticReducerAction<U>) => {
+    const reducer = reducerRef.current
+    const reducers = Array.isArray(reducer) ? reducer : [reducer]
+    setState((prevState) => {
+      return reducers.reduce(
+        (acc, reducer) =>
+          reducer(acc, {type: action.type, document: action.document, id: action.id}),
+        prevState,
+      )
+    })
+  }, [])
 
   const setStateFromEvent = useCallback(
     (event: {id: string; document: SanityDocumentBase}) =>
       setStateFromAction({
+        id: getPublishedId(event.id),
         // @todo You shall not cast
         document: event.document as U,
+        // @todo This should eventually be emitted by the state machine
         type: 'mutate',
-        id: getPublishedId(event.id),
       }),
     [setStateFromAction],
   )
