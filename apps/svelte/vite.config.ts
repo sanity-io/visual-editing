a0222fc9f5bfe61697858d77f142963510f5fb30
Cody Olsen <stipsan@gmail.com>
2024-09-17 15:40:16 +0200

    chore: add `@repo/studio-url`

--- apps/svelte/vite.config.ts
+++ apps/svelte/vite.config.ts
@@ -3,6 +3,4 @@ import {defineConfig} from 'vite'
 
 export default defineConfig({
   plugins: [sveltekit()],
-  // Fix process undefined when importing from apps-common/env
-  define: {'process.env': {}},
 })
