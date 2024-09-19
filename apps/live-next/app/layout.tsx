9e30756d9e3300e2b13181da03135a0ceb6effe7
Cody Olsen <stipsan@gmail.com>
2024-09-19 18:24:43 +0200

    chore: fix prod crash

--- apps/live-next/app/layout.tsx
+++ apps/live-next/app/layout.tsx
@@ -57,7 +57,7 @@ async function Footer() {
   return (
     <footer className="bg-accent-1 border-accent-2 border-t">
       <div className="container mx-auto px-5">
-        {footer.length > 0 && (
+        {footer?.length > 0 && (
           <PortableText
             className="prose-sm bottom-0 w-full max-w-none text-pretty bg-white py-12 text-center md:py-20"
             value={footer as PortableTextBlock[]}
