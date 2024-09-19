d1b1bcceae8c2ec02d521bd72c3ab5548ff51f59
Cody Olsen <stipsan@gmail.com>
2024-09-19 13:34:06 +0200

    chore: better memoization of animated letters

--- apps/live-next/app/page.tsx
+++ apps/live-next/app/page.tsx
@@ -20,9 +20,10 @@ function Intro(props: {title: string | null | undefined; description: any}) {
   return (
     <section className="mb-16 mt-16 flex flex-col items-center lg:mb-12 lg:flex-row lg:justify-between">
       {title && (
-        <AnimatedH1 className="text-balance text-6xl font-bold leading-tight tracking-tighter lg:pr-8 lg:text-8xl">
-          {title}
-        </AnimatedH1>
+        <AnimatedH1
+          className="text-balance text-6xl font-bold leading-tight tracking-tighter lg:pr-8 lg:text-8xl"
+          text={title}
+        />
       )}
       {description.length > 0 && (
         <h2 className="mt-5 text-pretty text-center text-lg lg:pl-8 lg:text-left">
