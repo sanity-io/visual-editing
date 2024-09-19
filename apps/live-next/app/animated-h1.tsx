c16ba367c669196a410b85ded90a702682da312c
Cody Olsen <stipsan@gmail.com>
2024-09-19 13:34:06 +0200

    chore: better memoization of animated letters

--- apps/live-next/app/animated-h1.tsx
+++ apps/live-next/app/animated-h1.tsx
@@ -2,71 +2,76 @@
 
 import {stegaClean} from '@sanity/client/stega'
 import {AnimatePresence, motion, Variants} from 'framer-motion'
-import {useEffect, useState} from 'react'
 
 interface CharacterData {
   char: string
   key: string
 }
 
-export function AnimatedH1({children, className}: {children: string; className: string}) {
-  const text = stegaClean(children)
+const containerVariants: Variants = {
+  hidden: {},
+  visible: (i = 1) => ({
+    transition: {
+      staggerChildren: 0.3 * i,
+      delayChildren: 0.4 * i,
+    },
+  }),
+  exit: (i = 1) => ({
+    transition: {
+      staggerChildren: 0.3 * i,
+      staggerDirection: -1,
+      delayChildren: 0.4 * i,
+    },
+  }),
+}
+
+const characterVariants: Variants = {
+  hidden: {
+    opacity: 0,
+    scale: 0,
+    width: 0,
+    transition: {type: 'spring', bounce: 0},
+  },
+  visible: {
+    opacity: 1,
+    y: 0,
+    scale: 1,
+    width: 'auto',
+    transition: {type: 'spring', bounce: 0},
+  },
+  exit: {
+    opacity: 0,
+    scale: 0,
+    width: 0,
+    transition: {type: 'spring', bounce: 0},
+  },
+}
+
+export function AnimatedH1({text: stegaText, className}: {text: string; className: string}) {
+  const text = stegaClean(stegaText)
   const transformChars = (text: string) => {
     const incs = new Map<string, number>()
     return text.split('').map((char) => {
       let n = incs.get(char) || 0
       const key = `${char}-${n++}`
       incs.set(char, n)
-      return {
-        char,
-        key,
-      } satisfies CharacterData
+      return (
+        <motion.span
+          key={key}
+          style={{display: 'inline-block', position: 'relative'}}
+          variants={characterVariants}
+          initial="hidden"
+          animate="visible"
+          exit="exit"
+          layout
+          aria-hidden="true"
+        >
+          {char === ' ' ? '\u00A0' : char}
+        </motion.span>
+      )
     })
   }
-  const [characters, setCharacters] = useState<CharacterData[]>(transformChars(text))
-
-  useEffect(() => {
-    setCharacters(transformChars(text))
-  }, [text])
-
-  const containerVariants: Variants = {
-    hidden: {},
-    visible: (i = 1) => ({
-      transition: {
-        staggerChildren: 0.3 * i,
-        delayChildren: 0.4 * i,
-      },
-    }),
-    exit: (i = 1) => ({
-      transition: {
-        staggerChildren: 0.3 * i,
-        staggerDirection: -1,
-        delayChildren: 0.4 * i,
-      },
-    }),
-  }
-
-  const characterVariants: Variants = {
-    hidden: {
-      opacity: 0,
-      scale: 0,
-      width: 0,
-      transition: {type: 'spring', bounce: 0},
-    },
-    visible: {
-      opacity: 1,
-      y: 0,
-      scale: 1,
-      width: 'auto',
-      transition: {type: 'spring', bounce: 0},
-    },
-    exit: {
-      opacity: 0,
-      scale: 0,
-      width: 0,
-      transition: {type: 'spring', bounce: 0},
-    },
-  }
+  const characters = transformChars(text)
 
   return (
     <>
@@ -78,24 +83,12 @@ export function AnimatedH1({children, className}: {children: string; className: 
         exit="exit"
         data-sanity-edit-target
       >
-        <span className="sr-only">{children}</span>
+        <span className="sr-only">{stegaText}</span>
         <AnimatePresence initial={false}>
-          {characters.map((charData) => (
-            <motion.span
-              key={charData.key}
-              style={{display: 'inline-block', position: 'relative'}}
-              variants={characterVariants}
-              initial="hidden"
-              animate="visible"
-              exit="exit"
-              layout
-              aria-hidden="true"
-            >
-              {charData.char === ' ' ? '\u00A0' : charData.char}
-            </motion.span>
-          ))}
+          {characters.map((character) => character)}
         </AnimatePresence>
       </motion.h1>
     </>
   )
 }
+AnimatedH1.displayName = 'AnimatedH1'
