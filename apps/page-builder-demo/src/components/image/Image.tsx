1473882f5161b9a69ab38fb63d0879e52a78a887
Snorre Eskeland Brekke <snorre.e.brekke@gmail.com>
2024-09-26 23:29:35 +0200

    Image fix (#1872)

--- apps/page-builder-demo/src/components/image/Image.tsx
+++ apps/page-builder-demo/src/components/image/Image.tsx
@@ -6,13 +6,17 @@ export function Image(
   props: {
     value: {
       _type: 'image'
-      asset: {_type: 'reference'}
+      asset?: {_type: 'reference'; _ref?: string}
     }
     width?: number
     height?: number
   } & Omit<HTMLProps<HTMLImageElement>, 'src' | 'value' | 'width' | 'height'>,
 ) {
   const {value, width = 800, height = 800, ...rest} = props
+
+  if (!value?.asset?._ref) {
+    return null
+  }
 
   return (
     // @ts-expect-error - NextImage is not typed correctly
