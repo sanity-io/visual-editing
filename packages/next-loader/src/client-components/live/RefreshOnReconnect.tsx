90188bfb7ecd53215d90b54ee647dd997ee1e5f6
Cody Olsen <stipsan@gmail.com>
2024-10-16 01:22:41 +0200

    feat(SanityLive): add `RefreshOnReconnect` support

--- packages/next-loader/src/client-components/live/RefreshOnReconnect.tsx
+++ packages/next-loader/src/client-components/live/RefreshOnReconnect.tsx
@@ -1,7 +1,21 @@
-// import {useRouter} from 'next/navigation.js'
+import {useRouter} from 'next/navigation.js'
+import {useEffect} from 'react'
 
 export default function RefreshOnReconnect(): null {
-  // const router = useRouter()
+  const router = useRouter()
+
+  useEffect(() => {
+    const controller = new AbortController()
+    const {signal} = controller
+    window.addEventListener(
+      'online',
+      () => {
+        router.refresh()
+      },
+      {passive: true, signal},
+    )
+    return () => controller.abort()
+  }, [router])
 
   return null
 }
