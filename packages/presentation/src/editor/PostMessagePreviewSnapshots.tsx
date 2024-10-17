f5ebe9080ad85ab25daf74500758bec28a4479d8
Rupert Dunk <rupert@rupertdunk.com>
2024-10-17 17:26:14 +0100

    fix(presentation): observe correct preview snapshot perspectives (#1947)

--- packages/presentation/src/editor/PostMessagePreviewSnapshots.tsx
+++ packages/presentation/src/editor/PostMessagePreviewSnapshots.tsx
@@ -1,5 +1,17 @@
+import type {ClientPerspective} from '@sanity/client'
 import {memo, useEffect, useMemo, type FC} from 'react'
-import {combineLatest, debounceTime, Subject, switchMap} from 'rxjs'
+import {
+  combineLatest,
+  debounceTime,
+  merge,
+  NEVER,
+  share,
+  skipWhile,
+  Subject,
+  switchMap,
+  takeUntil,
+} from 'rxjs'
+import {getDraftId, getPublishedId} from 'sanity'
 import {useDocumentPreviewStore, useSchema, type PreviewValue} from '../internals'
 import type {VisualEditingConnection} from '../types'
 
@@ -10,11 +22,12 @@ type Ref = {
 
 export interface PostMessagePreviewsProps {
   comlink: VisualEditingConnection
+  perspective: ClientPerspective
   refs: Ref[]
 }
 
 const PostMessagePreviews: FC<PostMessagePreviewsProps> = (props) => {
-  const {comlink, refs} = props
+  const {comlink, refs, perspective} = props
   const documentPreviewStore = useDocumentPreviewStore()
   const schema = useSchema()
 
@@ -24,12 +37,34 @@ const PostMessagePreviews: FC<PostMessagePreviewsProps> = (props) => {
     return refsSubject.asObservable().pipe(
       switchMap((refs) => {
         return combineLatest(
-          refs.map((ref) => documentPreviewStore.observeForPreview(ref, schema.get(ref._type)!)),
+          refs.map((ref) => {
+            const draftRef = {...ref, _id: getDraftId(ref._id)}
+            const draft$ =
+              perspective === 'previewDrafts'
+                ? documentPreviewStore
+                    .observeForPreview(draftRef, schema.get(draftRef._type)!)
+                    .pipe(
+                      // Share to prevent double subscribe in the merge
+                      share(),
+                      // Don't emit if no snapshot is returned
+                      skipWhile((p) => p.snapshot === null),
+                    )
+                : // Don't emit if not displaying drafts
+                  NEVER
+
+            const publishedRef = {...ref, _id: getPublishedId(ref._id)}
+            const published$ = documentPreviewStore.observeForPreview(
+              publishedRef,
+              schema.get(publishedRef._type)!,
+            )
+
+            return merge(published$.pipe(takeUntil(draft$)), draft$)
+          }),
         )
       }),
       debounceTime(0),
     )
-  }, [documentPreviewStore, refsSubject, schema])
+  }, [documentPreviewStore, refsSubject, schema, perspective])
 
   useEffect(() => {
     const sub = previews$.subscribe((snapshots) => {
@@ -38,7 +73,10 @@ const PostMessagePreviews: FC<PostMessagePreviewsProps> = (props) => {
         data: {
           snapshots: snapshots
             .filter((s) => s.snapshot)
-            .map((s) => s.snapshot as PreviewValue & {_id: string}),
+            .map((s) => {
+              const snapshot = s.snapshot as PreviewValue & {_id: string}
+              return {...snapshot, _id: getPublishedId(snapshot._id)}
+            }),
         },
       })
     })
