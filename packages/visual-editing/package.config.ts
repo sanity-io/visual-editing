a7d9f373a43313bbc7dc0e7a060b948b9fc906d0
Cody Olsen <stipsan@gmail.com>
2024-10-22 01:43:31 +0200

    refactor: only minify `@sanity/visual-editing`

--- packages/visual-editing/package.config.ts
+++ packages/visual-editing/package.config.ts
@@ -3,13 +3,13 @@ import {defineConfig} from '@sanity/pkg-utils'
 
 export default defineConfig({
   ...baseConfig,
+  minify: true,
   // `@sanity/visual-editing` isn't designed to be server side rendered
   runtime: 'browser',
   define: {
     'process.env.NODE_ENV': 'production',
   },
   rollup: {
-    ...baseConfig.rollup,
     treeshake: {
       preset: 'smallest',
       manualPureFunctions: ['createElement', 'forwardRef', 'memo', 'styled'],
