fae66c7eba0dcfd735bed3710985bb70ac7afb2d
Cody Olsen <stipsan@gmail.com>
2024-10-21 23:27:07 +0200

    refactor: remove `@repo/channels` as it's no longer used

--- packages/channels/README.md
+++
@@ -1,3 +0,0 @@
-> **Warning**
->
-> This is a private package that is bundled into other packages, it's not meant to be published to npm or be used directly.
--- packages/presentation/package.config.ts
+++ packages/presentation/package.config.ts
@@ -3,7 +3,6 @@ import {defineConfig} from '@sanity/pkg-utils'
 
 export default defineConfig({
   ...baseConfig,
-  minify: false,
   external: ['@sanity/ui', 'react', 'react-dom', 'sanity', 'styled-components'],
   define: {
     PRESENTATION_ENABLE_LIVE_DRAFT_EVENTS: process.env['PRESENTATION_ENABLE_LIVE_DRAFT_EVENTS'],
