5d85b31681f7313b721e79a7967f1d6bdcf1b72e
pedrobonamin <pedrobonamin@gmail.com>
2024-10-31 09:55:07 +0100

    chore(deps): update sanity corel to latest

--- apps/page-builder-demo/src/sanity.types.ts
+++ apps/page-builder-demo/src/sanity.types.ts
@@ -694,6 +694,30 @@ export type FrontPageQueryResult = null | {
   style: null
 }
 
+// Source: ./src/app/dnd/page.tsx
+// Variable: dndPageQuery
+// Query: *[_type == "dndTestPage"]{    _id,    title,    children  }[0]
+export type DndPageQueryResult = {
+  _id: string
+  title: string | null
+  children: Array<{
+    title?: string
+    childrenStrings?: Array<string>
+    children?: Array<{
+      title?: string
+      children?: Array<{
+        title?: string
+        _type: 'dndTestItemChildL2'
+        _key: string
+      }>
+      _type: 'dndTestItemChildL1'
+      _key: string
+    }>
+    _type: 'dndTestItem'
+    _key: string
+  }> | null
+} | null
+
 // Source: ./src/app/products/page.tsx
 // Variable: productsPageQuery
 // Query: *[_type == "product" && defined(slug.current)]{    _id,    title,    description,    slug,    "media": media[0]  }
@@ -895,30 +919,6 @@ export type PageSlugsResult = Array<{
   slug: string | null
 }>
 
-// Source: ./src/app/dnd/page.tsx
-// Variable: dndPageQuery
-// Query: *[_type == "dndTestPage"]{    _id,    title,    children  }[0]
-export type DndPageQueryResult = {
-  _id: string
-  title: string | null
-  children: Array<{
-    title?: string
-    childrenStrings?: Array<string>
-    children?: Array<{
-      title?: string
-      children?: Array<{
-        title?: string
-        _type: 'dndTestItemChildL2'
-        _key: string
-      }>
-      _type: 'dndTestItemChildL1'
-      _key: string
-    }>
-    _type: 'dndTestItem'
-    _key: string
-  }> | null
-} | null
-
 // Source: ./src/app/product/[slug]/page.tsx
 // Variable: productSlugsQuery
 // Query: *[_type == "product" && defined(slug.current)]{"slug": slug.current}
@@ -1039,11 +1039,11 @@ declare module '@sanity/client' {
   interface SanityQueries {
     '\n  *[_id == "siteSettings"][0]{\n  title,\n  description,\n  copyrightText\n}': LayoutQueryResult
     "\n  *[_id == \"siteSettings\"][0]{\n    frontPage->{\n      _type,\n      _id,\n      title,\n      sections[]{\n        ...,\n        symbol->{_type},\n        'headline': coalesce(headline, symbol->headline),\n        'tagline': coalesce(tagline, symbol->tagline),\n        'subline': coalesce(subline, symbol->subline),\n        'image': coalesce(image, symbol->image),\n        product->{\n          _type,\n          _id,\n          title,\n          slug,\n          \"media\": media[0]\n        },\n        products[]{\n          _key,\n          ...(@->{\n            _type,\n            _id,\n            title,\n            slug,\n            \"media\": media[0]\n          })\n        }\n      },\n      style\n    }\n  }.frontPage\n": FrontPageQueryResult
+    '\n  *[_type == "dndTestPage"]{\n    _id,\n    title,\n    children\n  }[0]\n': DndPageQueryResult
     '\n  *[_type == "product" && defined(slug.current)]{\n    _id,\n    title,\n    description,\n    slug,\n    "media": media[0]\n  }\n': ProductsPageQueryResult
     '*[_type == "project" && defined(slug.current)]': ProjectsPageQueryResult
     "\n  *[_type == \"page\" && slug.current == $slug][0]{\n    _type,\n    _id,\n    title,\n    sections[]{\n      ...,\n      symbol->{_type},\n      'headline': coalesce(headline, symbol->headline),\n      'tagline': coalesce(tagline, symbol->tagline),\n      'subline': coalesce(subline, symbol->subline),\n      'image': coalesce(image, symbol->image),\n      product->{\n        _type,\n        _id,\n        title,\n        slug,\n        \"media\": media[0]\n      },\n      products[]{\n        _key,\n        ...(@->{\n          _type,\n          _id,\n          title,\n          slug,\n          \"media\": media[0]\n        })\n      }\n    },\n    style\n  }\n": PageQueryResult
     '*[_type == "page" && defined(slug.current)]{"slug": slug.current}': PageSlugsResult
-    '\n  *[_type == "dndTestPage"]{\n    _id,\n    title,\n    children\n  }[0]\n': DndPageQueryResult
     '*[_type == "product" && defined(slug.current)]{"slug": slug.current}': ProductSlugsQueryResult
     '*[_type == "product" && slug.current == $slug][0]': ProductPageQueryResult
     '*[_type == "project" && defined(slug.current)]{"slug": slug.current}': ProjectSlugsQueryResult
