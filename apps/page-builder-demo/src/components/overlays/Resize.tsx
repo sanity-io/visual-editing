cd77a2821d7c84032cae95ec4c24afcb51ac5d9a
George Francis <georgedoescode@gmail.com>
2024-11-05 14:52:55 +0000

    tidy up styles

--- apps/page-builder-demo/src/components/overlays/Resize.tsx
+++ apps/page-builder-demo/src/components/overlays/Resize.tsx
@@ -1,21 +1,6 @@
 import {at, set} from '@sanity/mutate'
-import {get} from '@sanity/util/paths'
 import {useDocuments, type OverlayComponent} from '@sanity/visual-editing'
 import {useCallback, useEffect, useMemo, useRef, type MouseEvent} from 'react'
-
-function mapAndClamp(
-  value: number,
-  inMin: number,
-  inMax: number,
-  outMin: number,
-  outMax: number,
-): number {
-  // Map the value from the input range to the output range
-  const mappedValue = ((value - inMin) * (outMax - outMin)) / (inMax - inMin) + outMin
-
-  // Clamp the result to ensure it stays within the output range
-  return Math.max(outMin, Math.min(outMax, mappedValue))
-}
 
 function debounce<F extends (...args: Parameters<F>) => ReturnType<F>>(fn: F, timeout: number): F {
   let timer: ReturnType<typeof setTimeout>
@@ -66,9 +51,6 @@ export const Resize: OverlayComponent = (props) => {
   const doc = getDocument(node.id)
 
   const resizeDir = element.getAttribute('data-resize-dir') || 'rtl'
-  const mappingAttr = element.getAttribute('data-resize-map')
-
-  const mapping = mappingAttr ? mappingAttr.split(',').map((n) => ~~n) : null
 
   const handleMouseDown = (e: MouseEvent<HTMLDivElement>) => {
     origin.current = {
@@ -84,9 +66,7 @@ export const Resize: OverlayComponent = (props) => {
   const patch = useCallback(
     (width: number) => {
       doc.patch(
-        ({snapshot}) => {
-          const currentValue = get(snapshot, node.path)
-
+        () => {
           return [at(node.path, set(width))]
         },
         {commit: false},
@@ -105,21 +85,12 @@ export const Resize: OverlayComponent = (props) => {
       const deltaX =
         resizeDir === 'ltr' ? origin.current.x - e.clientX : e.clientX - origin.current.x
 
-      const mapped = !mapping
-        ? deltaX
-        : ~~mapAndClamp(deltaX, mapping[0], mapping[1], mapping[2], mapping[3])
-
-      console.log(mapped)
-
-      const newW = mapping ? mapped : bounds.width + deltaX
+      const newW = bounds.width + deltaX
 
       deferredPatch(newW)
       deferredCommit()
 
       element.style.userSelect = 'none'
-      // element.style.cursor = 'none'
-      // document.body.style.cursor = 'none'
-      // element.style.width = `${newW}px`
     }
 
     const handleMouseUp: EventListener = (e) => {
@@ -135,13 +106,19 @@ export const Resize: OverlayComponent = (props) => {
   return (
     <PointerEvents>
       <div
-        className="absolute h-4 w-4 cursor-pointer"
         onMouseDown={handleMouseDown}
         style={{
+          position: 'absolute',
+          width: '20px',
+          height: '20px',
+          cursor: 'pointer',
           bottom: '0px',
           left: resizeDir === 'ltr' ? '0px' : 'unset',
           right: resizeDir === 'rtl' ? '0px' : 'unset',
           background: '#556bfc',
+          borderTopRightRadius: resizeDir === 'ltr' ? '8px' : 'unset',
+          borderTopLeftRadius: resizeDir === 'rtl' ? '8px' : 'unset',
+          padding: '2px',
         }}
       >
         <svg
