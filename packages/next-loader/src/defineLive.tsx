4acf6c4e448ba893c854439432734246994e2b9a
Cody Olsen <stipsan@gmail.com>
2024-11-18 14:19:47 +0100

    fix: use the CDN when fetching sync tags

--- packages/next-loader/src/defineLive.tsx
+++ packages/next-loader/src/defineLive.tsx
@@ -201,6 +201,7 @@ export function defineLive(config: DefineSanityLiveOptions): {
         revalidate: fetchOptions?.revalidate,
         // tags: ['sanity'],
       },
+      useCdn: true,
       tag: [tag, 'fetch-sync-tags'].filter(Boolean).join('.'),
     })
 
