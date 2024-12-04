8389bf944889607faea06f520be102e505ea73d9
Cody Olsen <stipsan@gmail.com>
2024-12-04 02:18:51 +0100

    chore: fix cyclic dep issue

--- packages/insert-menu/package.config.ts
+++ packages/insert-menu/package.config.ts
@@ -1,9 +1,10 @@
-import baseConfig from '@repo/package.config'
 import {defineConfig} from '@sanity/pkg-utils'
 
 // https://github.com/sanity-io/pkg-utils#configuration
 export default defineConfig({
-  ...baseConfig,
   // the path to the tsconfig file for distributed builds
   tsconfig: 'tsconfig.dist.json',
+  rollup: {
+    optimizeLodash: true,
+  },
 })
