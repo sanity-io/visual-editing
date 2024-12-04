6c5092e9bf900eb8e3cad65001101b9893b8881b
George Francis <georgedoescode@gmail.com>
2024-12-04 12:57:14 +0000

    chore(dnd-telemetry): add sendVisualEditingTelemetry

---
+++ packages/visual-editing/src/ui/telemetry/sendVisualEditingTelemetry.ts
@@ -0,0 +1,30 @@
+import {defineEvent} from '@sanity/telemetry'
+import type {VisualEditingNode} from '../../types'
+
+const events = {
+  'Drag Sequence Complete': defineEvent({
+    name: 'Drag Sequence Complete',
+    description: 'An array is successfully reordered using drag and drop.',
+    version: 1,
+  }),
+}
+
+type EventDataMap = {
+  [K in keyof typeof events]: (typeof events)[K] extends ReturnType<typeof defineEvent<infer T>>
+    ? T
+    : never
+}
+
+function sendVisualEditingTelemetry<K extends keyof typeof events>(
+  name: K,
+  data: EventDataMap[K] extends void ? null | undefined : EventDataMap[K],
+  comlink: VisualEditingNode | undefined,
+) {
+  if (!comlink) return
+
+  const event = events[name]
+
+  comlink.post('visual-editing/telemetry-log', {event, data})
+}
+
+export {sendVisualEditingTelemetry}
