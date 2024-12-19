3b372fb184971567564944702515128e1f7d9d3f
Cody Olsen <stipsan@gmail.com>
2024-12-19 11:58:24 +0100

    chore: handle deprecated exports

--- packages/visual-editing/src/index.ts
+++ packages/visual-editing/src/index.ts
@@ -1,3 +1,17 @@
+import type {DatasetMutatorMachineInput as DatasetMutatorMachineInputDeprecated} from './optimistic/state/datasetMutator'
+import type {
+  DocumentsGet as DocumentsGetDeprecated,
+  DocumentsMutate as DocumentsMutateDeprecated,
+  OptimisticDocument as OptimisticDocumentDeprecated,
+  OptimisticDocumentPatches as OptimisticDocumentPatchesDeprecated,
+  OptimisticReducerAction as OptimisticReducerActionDeprecated,
+  OptimisticReducer as OptimisticReducerDeprecated,
+  Path as PathDeprecated,
+  PathValue as PathValueDeprecated,
+} from './optimistic/types'
+import {useDocuments as useDocumentsDeprecated} from './react/useDocuments'
+import {useOptimistic as useOptimisticDeprecated} from './react/useOptimistic'
+
 export {createOverlayController} from './controller'
 export type {
   DisableVisualEditing,
@@ -78,20 +92,74 @@ export {
 export {getArrayItemKeyAndParentPath} from './util/mutations'
 
 /**
- * Deprecated exports: React specific exports moved to `/react`. Generic
- * optimistic type exports moved to `/optimistic`.
- * @todo How can these be marked as deprecated OR remove and major?
+ * @public
+ * @deprecated Use `import {useDocuments} from '@sanity/visual-editing/react'` instead
  */
-export {useDocuments} from './react/useDocuments'
-export {useOptimistic} from './react/useOptimistic'
-export type {DatasetMutatorMachineInput} from './optimistic/state/datasetMutator'
+export const useDocuments = useDocumentsDeprecated
+/**
+ * @public
+ * @deprecated Use `import {useOptimistic} from '@sanity/visual-editing/react'` instead
+ */
+export const useOptimistic = useOptimisticDeprecated
+/**
+ * @public
+ * @deprecated Use `import type {DatasetMutatorMachineInput} from '@sanity/visual-editing/optimistic'` instead
+ */
+export type DatasetMutatorMachineInput = DatasetMutatorMachineInputDeprecated
+/**
+ * @public
+ * @deprecated Use `import type {DocumentsGet} from '@sanity/visual-editing/optimistic'` instead
+ */
+export type DocumentsGet = DocumentsGetDeprecated
+/**
+ * @public
+ * @deprecated Use `import type {DocumentsMutate} from '@sanity/visual-editing/optimistic'` instead
+ */
+export type DocumentsMutate = DocumentsMutateDeprecated
+/**
+ * @public
+ * @deprecated Use `import type {OptimisticDocument} from '@sanity/visual-editing/optimistic'` instead
+ */
+export type OptimisticDocument = OptimisticDocumentDeprecated
+/**
+ * @public
+ * @deprecated Use `import type {OptimisticDocumentPatches} from '@sanity/visual-editing/optimistic'` instead
+ */
+export type OptimisticDocumentPatches = OptimisticDocumentPatchesDeprecated
+/**
+ * @public
+ * @deprecated Use `import type {OptimisticReducer} from '@sanity/visual-editing/optimistic'` instead
+ */
+export type OptimisticReducer<T, U> = OptimisticReducerDeprecated<T, U>
+/**
+ * @public
+ * @deprecated Use `import type {OptimisticReducerAction} from '@sanity/visual-editing/optimistic'` instead
+ */
+export type OptimisticReducerAction<T> = OptimisticReducerActionDeprecated<T>
+/**
+ * @public
+ * @deprecated Use `import type {Path} from '@sanity/visual-editing/optimistic'` instead
+ */
+export type Path<T, K extends keyof T> = PathDeprecated<T, K>
+/**
+ * @public
+ * @deprecated Use `import type {PathValue} from '@sanity/visual-editing/optimistic'` instead
+ */
+export type PathValue<T, P extends string> = PathValueDeprecated<T, P>
+/**
+ * @internal
+ * @deprecated - do not use
+ */
 export type {
-  DocumentsGet,
-  DocumentsMutate,
-  OptimisticDocument,
-  OptimisticDocumentPatches,
-  OptimisticReducer,
-  OptimisticReducerAction,
-  Path,
-  PathValue,
-} from './optimistic/types'
+  useDocumentsDeprecated,
+  useOptimisticDeprecated,
+  DatasetMutatorMachineInputDeprecated,
+  DocumentsGetDeprecated,
+  DocumentsMutateDeprecated,
+  OptimisticDocumentDeprecated,
+  OptimisticDocumentPatchesDeprecated,
+  OptimisticReducerDeprecated,
+  OptimisticReducerActionDeprecated,
+  PathDeprecated,
+  PathValueDeprecated,
+}
