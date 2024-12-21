109fdd80d4c0bce5012eafe1807872de3c0d3105
Cody Olsen <stipsan@gmail.com>
2024-12-21 16:11:29 +0100

    chore: WIP

--- packages/visual-editing/src/ui/VisualEditing.tsx
+++ packages/visual-editing/src/ui/VisualEditing.tsx
@@ -1,4 +1,4 @@
-import {useEffect, useState, type FunctionComponent} from 'react'
+import {useSyncExternalStore, type FunctionComponent} from 'react'
 import type {VisualEditingOptions} from '../types'
 import {History} from './History'
 import {Meta} from './Meta'
@@ -7,47 +7,8 @@ import {Refresh} from './Refresh'
 import {useComlink} from './useComlink'
 import {useDatasetMutator} from './useDatasetMutator'
 
-if (typeof window !== 'undefined' && typeof document !== 'undefined') {
-  window.addEventListener(
-    'load',
-    () => {
-      // eslint-disable-next-line no-console
-      console.count('window.load')
-    },
-    {once: true},
-  )
-  window.addEventListener(
-    'DOMContentLoaded',
-    () => {
-      // eslint-disable-next-line no-console
-      console.count('window.DOMContentLoaded')
-    },
-    {once: true},
-  )
-  window.addEventListener(
-    'loadeddata',
-    () => {
-      // eslint-disable-next-line no-console
-      console.count('window.loadeddata')
-    },
-    {once: true},
-  )
-  window.addEventListener(
-    'loadedmetadata',
-    () => {
-      // eslint-disable-next-line no-console
-      console.count('window.loadedmetadata')
-    },
-    {once: true},
-  )
-  window.addEventListener(
-    'loadstart',
-    () => {
-      // eslint-disable-next-line no-console
-      console.count('window.loadstart')
-    },
-    {once: true},
-  )
+function emptySubscribe() {
+  return () => {}
 }
 
 /**
@@ -55,9 +16,11 @@ if (typeof window !== 'undefined' && typeof document !== 'undefined') {
  */
 export const VisualEditing: FunctionComponent<VisualEditingOptions> = (props) => {
   const {components, history, refresh, zIndex} = props
-  const [inFrame, setInFrame] = useState<boolean | null>(null)
-  useEffect(() => setInFrame(window.self !== window.top || window.opener), [])
-
+  const inFrame = useSyncExternalStore(
+    emptySubscribe,
+    () => window.self !== window.top || Boolean(window.opener),
+    () => null,
+  )
   const comlink = useComlink(inFrame === true)
   useDatasetMutator(comlink)
 
