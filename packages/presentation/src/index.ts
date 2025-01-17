d31c2aa5a565286636fb735a7a56f2dde6f30dde
Cody Olsen <81981+stipsan@users.noreply.github.com>
2025-01-17 22:20:51 +0100

    chore: remove `@sanity/presentation` workspace (#2525)

--- packages/presentation/src/index.ts
+++
@@ -1,6 +0,0 @@
-/**
- * @deprecated use `import {presentationTool} from 'sanity/presentation`
- */
-export function presentationTool(): void {
-  throw new Error("Use `import {presentationTool} from 'sanity/presentation'` instead")
-}
