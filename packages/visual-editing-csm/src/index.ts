855c4820d51e11bbf52395b7733aacfd14f81f87
Cody Olsen <stipsan@gmail.com>
2025-01-22 09:37:01 +0100

    fix!: remove `DRAFTS_PREFIX` export

--- packages/visual-editing-csm/src/index.ts
+++ packages/visual-editing-csm/src/index.ts
@@ -7,6 +7,5 @@ export {
 export {urlStringToPath} from './urlStringToPath'
 export {encodeSanityNodeData} from './encodeSanityNodeData'
 export {decodeSanityNodeData} from './decodeSanityNodeData'
-export {DRAFTS_PREFIX} from './constants'
 export {pathToUrlString} from './pathToUrlString'
 export type {SanityNode, SanityStegaNode} from '@sanity/visual-editing-types'
