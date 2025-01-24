faba700a64705c04136106110b311ad8f7730e24
Cody Olsen <81981+stipsan@users.noreply.github.com>
2025-01-24 11:21:03 +0100

    chore: remove `@sanity/preview-kit-compat` (#2556)

--- vitest.workspace.js
+++ vitest.workspace.js
@@ -4,7 +4,6 @@ export default defineWorkspace([
   './packages/presentation-comlink/vitest.config.ts',
   './packages/preview-url-secret/vitest.config.ts',
   './packages/react-loader/vitest.config.ts',
-  './packages/preview-kit-compat/vitest.config.ts',
   './packages/visual-editing/vitest.config.ts',
   './packages/core-loader/vitest.config.ts',
   './packages/svelte-loader/vitest.config.ts',
