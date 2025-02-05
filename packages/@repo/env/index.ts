1e6c0e51ecb1ffe95a8503ad3de410c27c278efa
Cody Olsen <81981+stipsan@users.noreply.github.com>
2025-02-05 14:08:50 +0100

    chore: prep testing corel

--- packages/@repo/env/index.ts
+++ packages/@repo/env/index.ts
@@ -15,7 +15,9 @@ export const datasets = {
   'blog': 'blog',
 } as const
 
-export const apiVersion = '2024-10-21' as const
+// @TODO uncomment once content releases is out of alpha
+// export const apiVersion = '2024-10-21' as const
+export const apiVersion = 'X' as const
 
 export const workspaces = {
   'astro': {
