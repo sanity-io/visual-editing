ce98771642bdedbd53bce50f597f7a6f3c62b6a1
Cody Olsen <stipsan@gmail.com>
2025-03-13 20:16:54 +0100

    fix: improve next.js pages router experience

--- packages/visual-editing/src/next-pages-router/VisualEditingComponent.tsx
+++ packages/visual-editing/src/next-pages-router/VisualEditingComponent.tsx
@@ -1,6 +1,12 @@
 import {useRouter} from 'next/router.js'
-import {useEffect, useRef, useState} from 'react'
-import type {HistoryAdapterNavigate, VisualEditingOptions} from '../types'
+import {useEffect, useState} from 'react'
+import {useEffectEvent} from 'use-effect-event'
+import type {
+  HistoryAdapterNavigate,
+  HistoryRefresh,
+  HistoryUpdate,
+  VisualEditingOptions,
+} from '../types'
 import {enableVisualEditing} from '../ui/enableVisualEditing'
 
 /**
@@ -14,15 +20,53 @@ export interface VisualEditingProps extends Omit<VisualEditingOptions, 'history'
 }
 
 export default function VisualEditingComponent(props: VisualEditingProps): null {
-  const {components, refresh, zIndex} = props
+  const {components, refresh: refreshProp, zIndex} = props
 
   const router = useRouter()
-  const routerRef = useRef(router)
   const [navigate, setNavigate] = useState<HistoryAdapterNavigate | undefined>()
 
-  useEffect(() => {
-    routerRef.current = router
-  }, [router])
+  const refresh = useEffectEvent((payload: HistoryRefresh): false | Promise<void> => {
+    if (refreshProp) {
+      return refreshProp(payload)
+    }
+    async function routerRefresh() {
+      // Using the pattern used in the next.js codebase: https://github.com/vercel/next.js/blob/ac2b8ebfb2068661e91ae0fce5039a2e528eb25e/test/integration/prerender-preview/pages/index.js#L31
+      await router.replace(router.asPath, undefined, {scroll: false, shallow: false})
+    }
+    const skipRefresh = (): false => {
+      // eslint-disable-next-line no-console
+      console.debug(
+        'Live preview is setup, mutation is skipped assuming its handled by the live preview',
+      )
+      return false
+    }
+    const mutationRefresh = () => {
+      // eslint-disable-next-line no-console
+      console.debug('No loaders in live mode detected, reloading server props')
+      return routerRefresh()
+    }
+
+    switch (payload.source) {
+      case 'manual':
+        return routerRefresh()
+      case 'mutation':
+        return payload.livePreviewEnabled ? skipRefresh() : mutationRefresh()
+      default:
+        throw new Error('Unknown refresh source', {cause: payload})
+    }
+  })
+  const update = useEffectEvent((event: HistoryUpdate) => {
+    switch (event.type) {
+      case 'push':
+        return router.push(event.url)
+      case 'pop':
+        return router.back()
+      case 'replace':
+        return router.replace(event.url)
+      default:
+        throw new Error(`Unknown event type: ${event.type}`)
+    }
+  })
   useEffect(() => {
     const disable = enableVisualEditing({
       components,
@@ -33,22 +77,11 @@ export default function VisualEditingComponent(props: VisualEditingProps): null 
           setNavigate(() => _navigate)
           return () => setNavigate(undefined)
         },
-        update: (update) => {
-          switch (update.type) {
-            case 'push':
-              return routerRef.current.push(update.url)
-            case 'pop':
-              return routerRef.current.back()
-            case 'replace':
-              return routerRef.current.replace(update.url)
-            default:
-              throw new Error(`Unknown update type: ${update.type}`)
-          }
-        },
+        update,
       },
     })
     return () => disable()
-  }, [components, refresh, zIndex])
+  }, [components, zIndex])
 
   const {asPath, basePath, locale, isReady} = useRouter()
   useEffect(() => {
