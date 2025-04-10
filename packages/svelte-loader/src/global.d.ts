83f1f47bec01f20c4065944f000dc712e49f9a3b
Cody Olsen <stipsan@gmail.com>
2025-04-11 01:04:07 +0200

    chore: fix linter

--- packages/svelte-loader/src/global.d.ts
+++ packages/svelte-loader/src/global.d.ts
@@ -2,6 +2,7 @@ import {LoaderLocals} from './types'
 
 declare global {
   namespace App {
+    // eslint-disable-next-line @typescript-eslint/no-empty-object-type
     interface Locals extends LoaderLocals {}
   }
 }
