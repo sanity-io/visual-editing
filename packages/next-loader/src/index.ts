59f75a041ca444f281018acb134d32e4f94bd5c3
Cody Olsen <stipsan@gmail.com>
2025-09-25 15:02:53 +0200

    refactor: `@sanity/next-loader` is retired <3

--- packages/next-loader/src/index.ts
+++
@@ -1,7 +0,0 @@
-/**
- * @public
- * @deprecated use `next-sanity/live` instead
- */
-export function defineLive(): void {
-  throw new Error('defineLive is now part of next-sanity/live and @sanity/next-loader is retired')
-}
