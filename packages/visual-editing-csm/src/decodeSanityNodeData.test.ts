cda6075de5752711d45b2c851baafc3edc8fe609
Cody Olsen <stipsan@gmail.com>
2025-10-16 21:51:23 +0200

    fix: handle `perspective` for "Open in Studio" links

--- packages/visual-editing-csm/src/decodeSanityNodeData.test.ts
+++ packages/visual-editing-csm/src/decodeSanityNodeData.test.ts
@@ -1,5 +1,5 @@
-import {expect, test} from 'vitest'
-import {decodeSanityString} from './decodeSanityNodeData'
+import {describe, expect, test} from 'vitest'
+import {decodeSanityString, decodeSanityNodeData} from './decodeSanityNodeData'
 
 test('an encoded string returns node data', async () => {
   const input = decodeSanityString(
@@ -16,4 +16,50 @@ test('an encoded string returns node data', async () => {
   }
 
   expect(input).toMatchObject(output)
+})
+
+describe('decodeSanityNodeData', () => {
+  test.each([
+    [
+      {
+        origin: 'sanity.io',
+        href: '/intent/edit/mode=presentation;id=e1674ab7-5f96-49b0-8291-7608e087ef0a;type=author;path=picture?baseUrl=%2F&id=e1674ab7-5f96-49b0-8291-7608e087ef0a&type=author&path=picture.alt&perspective=rrRHoDGSr',
+      },
+      {
+        baseUrl: '/',
+        id: 'e1674ab7-5f96-49b0-8291-7608e087ef0a',
+        type: 'author',
+        path: 'picture.alt',
+        perspective: 'rrRHoDGSr',
+      },
+    ],
+    [
+      {
+        origin: 'sanity.io',
+        href: '/intent/edit/mode=presentation;id=f99b873f-85c2-41c4-ac65-807b1d72c084;type=author;path=picture?baseUrl=%2F&id=f99b873f-85c2-41c4-ac65-807b1d72c084&type=author&path=picture.alt',
+      },
+      {
+        baseUrl: '/',
+        id: 'f99b873f-85c2-41c4-ac65-807b1d72c084',
+        type: 'author',
+        path: 'picture.alt',
+        perspective: 'drafts',
+      },
+    ],
+    [
+      {
+        origin: 'sanity.io',
+        href: '/intent/edit/mode=presentation;id=3db5cb7b-bfd2-4409-a42e-86e92def3098;type=post;path=coverImage?baseUrl=%2F&id=3db5cb7b-bfd2-4409-a42e-86e92def3098&type=post&path=coverImage.alt&perspective=published',
+      },
+      {
+        baseUrl: '/',
+        id: '3db5cb7b-bfd2-4409-a42e-86e92def3098',
+        type: 'post',
+        path: 'coverImage.alt',
+        perspective: 'published',
+      },
+    ],
+  ])('%j => %j', (input, output) => {
+    expect(decodeSanityNodeData(input)).toEqual(output)
+  })
 })
