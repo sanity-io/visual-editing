8ee9b2dda4eefe5635356109eeda05043110cb7c
Cody Olsen <stipsan@gmail.com>
2025-10-16 18:50:34 +0200

    fix: comlink has moved to a new repo

--- packages/presentation-comlink/src/isMaybePresentation.ts
+++
@@ -1,9 +0,0 @@
-export function isMaybePreviewIframe(): boolean {
-  return window.self !== window.top
-}
-export function isMaybePreviewWindow(): boolean {
-  return Boolean(window.opener)
-}
-export function isMaybePresentation(): boolean {
-  return isMaybePreviewIframe() || isMaybePreviewWindow()
-}
