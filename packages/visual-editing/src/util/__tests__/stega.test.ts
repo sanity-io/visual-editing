37d55d8d508b7a0206f65ce30f8ccaa5fabbaeae
Cody Olsen <stipsan@gmail.com>
2026-03-19 13:38:03 +0100

    fix(stega): fails decoding content containing zero width space chars

---
+++ packages/visual-editing/src/util/__tests__/stega.test.ts
@@ -0,0 +1,15 @@
+import {expect, test} from 'vitest'
+
+import {vercelStegaCombine} from '@vercel/stega'
+import {testAndDecodeStega} from '../stega'
+
+test('it handles trailing zero-width space characters', () => {
+  const payload = 'foo.\u200b'
+  const editInfo = {origin: 'sanity.io', href: '/studio'}
+  const encoded = vercelStegaCombine(payload, editInfo)
+  expect(encoded).not.toEqual(payload)
+  const decoded = testAndDecodeStega(encoded)
+  expect(decoded).not.toEqual(null)
+  expect(decoded!.origin).toEqual('sanity.io')
+  expect(decoded!.href).toEqual('/studio')
+})
